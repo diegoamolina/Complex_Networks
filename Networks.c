35ee309f84c65db64bc8b50bb8342faa94733c41
Diegosis <55146772+Diegosis91@users.noreply.github.com>
2021-09-29 23:12:00 -0300

    Upload to cpp

--- Networks.c
+++ Networks.c
@@ -4,10 +4,10 @@
 #include "Test Network 1.c"
 #include "Functions.c"
 
-void main ()
+int main (void)
 {
     int first_node, second_node, final_distance;
-    
+
     printf("Insert one node of network\n");
     scanf("%d",first_node);
 
@@ -15,6 +15,8 @@ void main ()
     scanf("%d", second_node);
 
     final_distance = distance(network, first_node, second_node);
-    printf("The distance between node %d and node %d is %d\n", first_node, second_node, final_distance)
+    printf("The distance between node %d and node %d is %d\n", first_node, second_node, final_distance);
+
+    return 0;
 
 }
